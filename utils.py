04fe13598b0cdf555e05b9c82820cb620bef2fc1
Rose <r-richardson@hotmail.co.uk>
2022-07-30 14:09:22 +0200

    added to-from methods to Words

--- utils.py
+++ utils.py
@@ -16,6 +16,41 @@ class Words:
     def __len__(self) -> int:
         return len(self._buf)
 
+    def to_int(self, endian: str):
+        temp = 0
+
+        if endian == 'big':
+            n = len(self)
+            for i in range(n):
+                temp += self[n - i - 1] << (16 * i)
+            return temp
+        
+        elif endian == 'little':
+            for i in range(len(self)):
+                temp += self[i] << (16 * i)
+            return temp
+
+        else:
+            raise NotImplementedError
+
+    @classmethod
+    def from_int(cls, value: int, endian: str, length: int):
+        ''' Get Words from int, result may be truncated if length is too short '''
+        temp = [0] * length
+
+        if endian == 'big':
+            for i in range(length):
+                temp[i] = ((0xFFFF << 16*(length - i - 1)) & value) >> 16*(length - i - 1)
+            return Words(temp)
+        
+        elif endian == 'little':
+            for i in range(length):
+                temp[i] = ((0xFFFF << 16*i) & value) >> 16*i
+            return Words(temp)
+        
+        else:
+            raise NotImplementedError
+
     @classmethod
     def from_bytes(cls, bytes_src: bytes, endian: str):
         assert isinstance(bytes_src, bytes)
@@ -47,10 +82,13 @@ class Words:
                 temp.append((self[i] & 0xFF00) >> 8)
                 temp.append((self[i] & 0x00FF) >> 0)
 
-        if endian == 'little':
+        elif endian == 'little':
             for i in range(len(self)):
                 temp.append((self[i] & 0x00FF) >> 0)
                 temp.append((self[i] & 0xFF00) >> 8)
+
+        else:
+            raise NotImplementedError
 
         return bytes(temp)
 
@@ -73,3 +111,9 @@ if __name__ == "__main__":
 
     words_from_odd_number_bytes = Words.from_bytes(bytes((0xFF, 0xDE, 0x55)), 'big')
     print(words_from_odd_number_bytes)
+
+    words_from_int = Words.from_int(0xFF778899, endian='big', length=2)
+    print(words_from_int)
+
+    words_to_int = words_from_int.to_int(endian='little')
+    print(hex(words_to_int))
